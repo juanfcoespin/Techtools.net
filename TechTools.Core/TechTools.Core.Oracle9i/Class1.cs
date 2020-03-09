80a55346c4cf5ff15e5f5c5e510d2ce8f8280698
Juan Francisco Espín Navia <juan.espin@yahoo.com>
2020-03-09 09:35:54 -0500

    09mar2020

---
+++ TechTools.Core/TechTools.Core.Oracle9i/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechTools.Core.Oracle9i
+{
+    public class Class1
+    {
+    }
+}
